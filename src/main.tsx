9f1b257935f1873d56fb4b2595bd7f97167347cb
ashwani8090 <ashwani8090singh@gmail.com>
2024-09-08 20:51:07 +0530

    added redux persist

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App.tsx";
 import { AuthProvider } from "./contexts/AuthProvider.tsx";
 import "./index.css";
-import { store } from "./store/index.tsx";
+import { persistor, store } from "./store/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </PersistGate>
   </Provider>,
 );
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,14 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
 import portfolioReducer from "./userSlice";
 
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["portfolio"], // only navigation will be persisted
+};
+
+const rootReducer = combineReducers({
+  portfolio: portfolioReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    portfolio: portfolioReducer,
-  },
+  reducer: persistedReducer,
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
